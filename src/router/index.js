413ad54da73158548db7cea681aca798539bdf8b
Benjamin Sabardu <bsabardu@gmail.com>
2021-01-05 10:26:53 +0100

    Add article views

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,13 @@ const router = require('express').Router();
 //Import Business Logic
 const services = require('../services')
 
-router.get("/", (req,res)=> {
-services.showHome(req,res);
+router.get("/", (req, res) => {
+    services.showHome(req, res);
+});
+
+router.get("/article/:id", (req, res) => {
+    services.showArticle(req, res);
 });
 
 
-module.exports = router; 
+module.exports = router;
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,14 @@
 const article = require('../../data/article.json');
 
 const services = {
-    showHome: function(req,res){
+    showHome(req,res){
         res.render('index', {articles : article});
-    } 
+    },
+    
+    showArticle(req,res){
+        let articleFound = article.find(article => article.id === parseInt(req.params.id, 10));
+        res.render('article', {article : articleFound});
+    }
 }
 
 module.exports = services;
