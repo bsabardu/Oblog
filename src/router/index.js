724bb6a7beb0c020a4106b6280e9bae932b5ded0
Benjamin Sabardu <bsabardu@gmail.com>
2021-01-05 11:08:15 +0100

    fix little issues, add page category

--- src/router/index.js
+++ src/router/index.js
@@ -12,5 +12,9 @@ router.get("/article/:id", (req, res) => {
     services.showArticle(req, res);
 });
 
+router.get("/category/:category", (req, res) => {
+    services.showCategory(req, res);
+});
+
 
 module.exports = router;
--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,18 @@
-const article = require('../../data/article.json');
+const articles = require('../../data/article.json');
 
 const services = {
     showHome(req,res){
-        res.render('index', {articles : article});
+        res.render('index', {articles : articles});
     },
     
     showArticle(req,res){
-        let articleFound = article.find(article => article.id === parseInt(req.params.id, 10));
+        let articleFound = articles.find(article => article.id === parseInt(req.params.id, 10));
         res.render('article', {article : articleFound});
+    },
+
+    showCategory(req,res){
+        let articlesCategory = articles.filter(article => article.category === req.params.category);
+        res.render('category', {articles : articlesCategory, category : req.params.category})
     }
 }
 
